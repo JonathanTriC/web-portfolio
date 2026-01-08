8c933bfb221200298124ffd854c2f13c21b9d9bf
Jonathan Tri <jonathantric@gmail.com>
2026-01-08 14:57:46 +0700

    chore: update cv link

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -111,7 +111,7 @@ export default function ContactForm() {
 										Curriculum Vitae
 									</h3>
 									<a
-										href="https://drive.google.com/file/d/1sHLeECuW8CMIMYEmTrs6NGXnbzhYGveZ/view?usp=sharing"
+										href="https://bit.ly/cv-jonathan-tri"
 										target="_blank"
 										className="text-neon text-base underline sm:text-lg"
 									>
